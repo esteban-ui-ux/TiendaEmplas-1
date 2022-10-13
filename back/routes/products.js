aa30ebb46f1cd797a773ca77692ff0a3743dd719
JessicaRamirezT <111193935+JessicaRamirezT@users.noreply.github.com>
2022-10-12 21:47:49 -0500

    Actualizacion de routes
    Actualización de rutas

--- back/routes/products.js
+++ back/routes/products.js
@@ -1,7 +1,10 @@
-const express = require("express");
-const router = express.Router();
+const express=require("express")
+const router=express.Router();
 
-const { getProducts } = require("../controllers/productsControllers.js"); //traemos el json desde el controlador
+const{getProducts, newProduct, getProductById}=require("../controllers/productsController")
 
-router.route("/productos").get(getProducts); //establecemos la ruta del get
-module.exports = router;
+router.route('/productos').get(getProducts)//establecemos ruta del get
+router.route('/producto/nuevo').post(newProduct)//establecemos la ruta del producto nuevo creado
+router.route('/producto/:id').get(getProductById)//ruta para consultar producto por id
+
+module.exports=router;
