3180467c8f5b50b9805ecd84089e4edf8c543398
ImberCarmina <110949680+imbercarmina@users.noreply.github.com>
2022-10-28 21:57:54 -0500

    Update products.js

--- back/routes/products.js
+++ back/routes/products.js
@@ -3,8 +3,8 @@ const router=express.Router();
 
 const{getProducts, newProduct, getProductById}=require("../controllers/productsController")
 
-router.route('/productos').get(getProducts)//establecemos ruta del get
-router.route('/producto/nuevo').post(newProduct)//establecemos la ruta del producto nuevo creado
-router.route('/producto/:id').get(getProductById)//ruta para consultar producto por id
+router.route('/productos').get(getProducts)//Se establece: ruta del get
+router.route('/producto/nuevo').post(newProduct)//Se establece: la ruta- producto nuevo creado
+router.route('/producto/:id').get(getProductById)//Ruta destinada a consultar producto por id
 
 module.exports=router;
